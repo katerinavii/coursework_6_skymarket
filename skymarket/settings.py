2247f75db08ab16dc066a1e7be992c346f31718f
Екатерина Шматко <shmatkokat@outlook.com>
2022-09-08 21:08:24 +0300

    Подключена БД sqlite3 вместо postgres.

--- skymarket/settings.py
+++ skymarket/settings.py
@@ -105,12 +105,12 @@ DJOSER = {
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'databasemarket.sqlite3',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'postgres',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
